7e7bdb7e8e9a8c97e4dd92263db7bc5363888078
Rahul-NITD <96250420+Rahul-NITD@users.noreply.github.com>
2023-03-01 19:15:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 import sys
+from Dice import Dice
 
-TILESIZE = 60
+TILESIZE = 50
+FPS = 60
 SIZE = WIDTH,HEIGHT = (25 * TILESIZE, 15 * TILESIZE)
 SCREEN = pygame.display.set_mode(SIZE, pygame.SRCALPHA)
+CLOCK = pygame.time.Clock()
+
+dice = Dice(TILESIZE)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    
+        
     SCREEN.fill((0,0,0,0))
+    SCREEN.blit(dice.DICESURFACE, (15*TILESIZE, 0, dice.WIDTH, dice.HEIGHT))
 
     pygame.display.flip()
+    CLOCK.tick(FPS)
