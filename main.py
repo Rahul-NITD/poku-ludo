b09e6233fd7532ade3b155c9ecca0a446969b142
Rahul-NITD <96250420+Rahul-NITD@users.noreply.github.com>
2023-03-01 18:49:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+TILESIZE = 60
+SIZE = WIDTH,HEIGHT = (25 * TILESIZE, 15 * TILESIZE)
+SCREEN = pygame.display.set_mode(SIZE, pygame.SRCALPHA)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+    SCREEN.fill((0,0,0,0))
+
+    pygame.display.update()
