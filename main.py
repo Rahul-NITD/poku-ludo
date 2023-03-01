c351a2689a7ec42615d345b157d54153eae811f8
Rahul-NITD <96250420+Rahul-NITD@users.noreply.github.com>
2023-03-01 20:22:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,29 @@ CLOCK = pygame.time.Clock()
 
 dice = Dice(TILESIZE)
 
+# load screen
+# def load_screen():
+#     dice.loadGIFS()
+#     while not dice.loaded:
+#         print(dice.loaded)
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 sys.exit()
+#         SCREEN.fill((255,255,255,255))
+
+        
+# load_screen()
+
+# play game
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        
+
+    # Update 
+    dice.update()
+
+    # Draw
     SCREEN.fill((0,0,0,0))
     SCREEN.blit(dice.DICESURFACE, (15*TILESIZE, 0, dice.WIDTH, dice.HEIGHT))
 
